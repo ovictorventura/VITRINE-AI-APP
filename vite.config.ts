648194af0fce02d3b38740ae7d9ad3c3ef4c5a11
ovictorventura <oventuravictor@gmail.com>
2025-09-05 22:06:39 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,27 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    const env = loadEnv(mode, '.', '');
+    return {
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      },
+      // ▼▼▼ ADICIONE ESTE BLOCO DE CÓDIGO ABAIXO ▼▼▼
+      preview: {
+        host: true,
+        port: 8080,
+        strictPort: true,
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+      server: {
+        host: true,
+        strictPort: true,
+        port: 8080, 
       }
-    };
+    };
 });
