766a2bbc624d73cb766a78698c0a0437ea79306e
ovictorventura <oventuravictor@gmail.com>
2025-09-05 22:12:24 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,12 @@ export default defineConfig(({ mode }) => {
           '@': path.resolve(__dirname, '.'),
         }
       },
-      // ▼▼▼ ADICIONE ESTE BLOCO DE CÓDIGO ABAIXO ▼▼▼
       preview: {
         host: true,
         port: 8080,
         strictPort: true,
+        // ▼▼▼ A LINHA QUE FALTAVA ESTÁ AQUI ▼▼▼
+        allowedHosts: ['vitrine-ai-app.onrender.com'], 
       },
       server: {
         host: true,
